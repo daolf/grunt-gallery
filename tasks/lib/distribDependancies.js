09f017f485cac69e20cd80995fc5ee3aa8e33ca8
Pierre De Wulf <pdewulf@lyra-network.com>
2015-06-05 10:23:49 +0200

    fixing path to enbale usage in frontfusion

--- tasks/lib/distribDependancies.js
+++ tasks/lib/distribDependancies.js
@@ -1,24 +1,24 @@
 module.exports = {
     /* list of dependencies for the future index.html of the gallery*/
     index : {
-        js : ['./node_modules/jquery/dist/jquery.js',
-              './node_modules/typeahead.js/dist/typeahead.bundle.min.js',
-              './node_modules/bootstrap/dist/js/bootstrap.min.js',
-              './views/js/search.js'],
-        css : ['./node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
-               './node_modules/bootstrap/dist/css/bootstrap.min.css',
-               './views/css/index.css'],
+        js : [__dirname+'/../../node_modules/jquery/dist/jquery.js',
+              __dirname+'/../../node_modules/typeahead.js/dist/typeahead.bundle.min.js',
+              __dirname+'/../../node_modules/bootstrap/dist/js/bootstrap.min.js',
+              __dirname+'/../../views/js/search.js'],
+        css : [__dirname+'/../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+               __dirname+'/../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+               __dirname+'/../../views/css/index.css'],
     },
     /* list of dependencies for the future page component pages of the gallery */
     gallery : {
-        js : ['./node_modules/jquery/dist/jquery.js',
-              './node_modules/codemirror/lib/codemirror.js',
-              './node_modules/bootstrap/dist/js/bootstrap.min.js',
-              './node_modules/codemirror/mode/javascript/javascript.js'],
-        
-        css : ['./node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
-               './node_modules/bootstrap/dist/css/bootstrap.min.css',
-               './node_modules/codemirror/lib/codemirror.css',
-               './views/css/gallery.css'],
+        js : [__dirname+'/../../node_modules/jquery/dist/jquery.js',
+              __dirname+'/../../node_modules/codemirror/lib/codemirror.js',
+              __dirname+'/../../node_modules/bootstrap/dist/js/bootstrap.min.js',
+              __dirname+'/../../node_modules/codemirror/mode/javascript/javascript.js'],
+
+        css : [__dirname+'/../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+               __dirname+'/../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+               __dirname+'/../../node_modules/codemirror/lib/codemirror.css',
+               __dirname+'/../../views/css/gallery.css'],
     }
 };
