d3f1e9653fc6797dacab9410f8ca39866320131e
Pierre De Wulf <pdewulf@lyra-network.com>
2015-07-07 16:08:56 +0200

    remove merge and push to master if success

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 branch=$TRAVIS_BRANCH
-git config --global credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 echo "on branch $branch"
 version=${branch##*/}
 echo "version is $version"
-echo "checkout master"
-git checkout master
-echo "merge $branch"
-git merge --no-ff $branch
 echo "git tag $version"
 git tag "$version"
-
-git push --tags origin master
