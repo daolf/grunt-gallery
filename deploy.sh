532b48e5d231e5514f0f2c28b83f7bad3ebcc1f0
Pierre De Wulf <pdewulf@lyra-network.com>
2015-07-08 11:50:04 +0200

    add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Running deployment script..."
+ 
+CURRENT_COMMIT=`git rev-parse HEAD`
+branch=$TRAVIS_BRANCH
+version=${branch##*/}
+
+# Change the branch used if applicable (e.g. gh-pages)
+echo "Cloning master branch..."
+ 
+# Hide output since we use an access token here
+git clone -b master "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1 || exit 1
+
+
+echo "Committing and pushing to GH"
+ 
+git config user.name "Travis-CI"
+git config user.email "travis@grunt-gallery.com"
+ 
+git checkout master
+git merge --no-ff $branch
+echo "git tag $version"
+# Commit changes, allowing empty changes (when unchanged)
+git add -A .
+git commit --allow-empty -m "Travis $TRAVIS_BUILD_NUMBER" || exit 1
+ 
+# Push to branch
+git push --tags origin master > /dev/null 2>&1 || exit 1
+ 
+echo "Pushed deployment successfully"
+exit 0
